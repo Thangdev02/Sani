6b8a107c5e91d3e24f54c1c5213e58a46f27ee07
Thangdev02 <thangdev92@gmail.com>
2025-09-08 16:54:32 +0700

    lendumt

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 // vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000", // JSON Server khi dev
-    },
+  base: './',       // <- quan trọng để relative paths
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') }
   },
-});
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
